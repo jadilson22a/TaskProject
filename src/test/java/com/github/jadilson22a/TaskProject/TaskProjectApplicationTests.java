05cca2972ef967c265d350af472dd2114df25f81
jadilson22a <jadilsonalves777.ma@outlook.com>
2026-05-01 19:32:50 -0300

    Iniciando projeto

---
+++ src/test/java/com/github/jadilson22a/TaskProject/TaskProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jadilson22a.TaskProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
